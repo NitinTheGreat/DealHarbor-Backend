ece2980ae8631e34e58c52f87902756f5ec588f2
NitinTheGreat <nitinpandey1304@gmail.com>
2025-06-23 17:23:08 +0530

    Initial Commit

---
+++ src/main/java/com/dealharbor/dealharbor_backend/DealharborBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dealharbor.dealharbor_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealharborBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealharborBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dealharbor/dealharbor_backend/DealharborBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dealharbor.dealharbor_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealharborBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
